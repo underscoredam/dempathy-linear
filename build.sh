573d3c54d3be73e380d107b2f0e9a6aa4e5d465b
Geek <underscoredam@gmail.com>
2016-04-12 13:24:12 +0545

    renamed "Empathy" to "empathy" from dempathy-app

--- build.sh
+++ build.sh
@@ -2,10 +2,5 @@
 
 cd dist
 
-if [ "$1" = "lib" ]; then
-	echo "building shared library"
-	cmake .. -DLIB=ON && make
-else
-	echo "building executable"
-	cmake .. -DLIB=OFF && make
-fi;
+cmake .. && make
+
